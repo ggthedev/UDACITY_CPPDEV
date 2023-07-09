76bce446ede2379d16b0ed81e30399c0184ca533
Gaurav Gupta <1276964+ggthedev@users.noreply.github.com>
2023-07-10 05:13:28 +0530

    updated code
    added std headers for fstream, sstream
    added input file path
    added function to read file and print its content
    added function which will generate and return a vector from a given string {0,1,0,0,}

--- src/udacppdev.h
+++ src/udacppdev.h
@@ -6,9 +6,17 @@
 #define UDACPPDEV_H
 
 #include <iostream>
+#include <fstream>
 #include <vector>
+#include <sstream>
+#include <string>
+#include <cstdio>
+
 using namespace std;
 
 #define BOARD_VALUES {{0, 1, 0, 0, 0, 0},{0, 1, 0, 0, 0, 0},{0, 1, 0, 0, 0, 0},{0, 1, 0, 0, 0, 0},{0, 0, 0, 0, 1, 0}};
+const string BOARD_FILE_NAME = "1.board";
+const string BOARD_FILE_PATH = string("./../../src/").append(BOARD_FILE_NAME);
+void TestParseLine();
 
 #endif //UDACPPDEV_H
