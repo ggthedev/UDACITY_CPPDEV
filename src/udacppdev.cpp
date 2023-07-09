f998cb928cfca4bad43d9f9cf2060c93dd17f863
Gaurav Gupta <1276964+ggthedev@users.noreply.github.com>
2023-07-10 02:51:51 +0530

    added few comments

--- src/udacppdev.cpp
+++ src/udacppdev.cpp
@@ -5,8 +5,12 @@
 #include "udacppdev.h"
 
 int main (){
-    vector<vector<int>> board = BOARD_VALUES;
-    for (const auto &i:board){
+    vector<vector<int>> board = BOARD_VALUES; //declares a vector of vector
+    // for loop with an auto iterator (const reference) since it's only copied in inner loop as an iterator
+    // from clang-tidy rules
+
+    for (const auto& i:board){
+        // for loop which iterates inner vector
         for (auto j:i){
             cout<<j;
         }
