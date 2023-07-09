a84d0a605c434acf3311e3587df821f75b07d363
Gaurav Gupta <1276964+ggthedev@users.noreply.github.com>
2023-07-10 01:08:06 +0530

    code formatting done

--- src/vectorsIntro.cpp
+++ src/vectorsIntro.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
-int main()
-{
-  vector<int> num = {1,2,3,4};
-for (auto i:num)
-{
-  cout<<i;
-}
-  return 0;
+int main() {
+    vector<int> num = {1, 2, 3, 4};
+    for (auto i: num) {
+        cout << i;
+    }
+    return 0;
 }
 
