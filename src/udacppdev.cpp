4c6727d89198445d452abb1ca2911648b58c41af
Gaurav Gupta <1276964+ggthedev@users.noreply.github.com>
2023-07-10 05:13:28 +0530

    updated code
    added std headers for fstream, sstream
    added input file path
    added function to read file and print its content
    added function which will generate and return a vector from a given string {0,1,0,0,}

--- src/udacppdev.cpp
+++ src/udacppdev.cpp
@@ -4,20 +4,70 @@
 
 #include "udacppdev.h"
 
-int main (){
-    vector<vector<int>> board = BOARD_VALUES; //declares a vector of vector
-    cout<<"Size of the board vector is "<<board.size()<<endl;
-    /*
-     * from clang-tidy rules: for loop with an auto iterator (const reference) since it's only copied in
-     * inner loop as an iterator and is a constant reference.
-    */
-    for (const auto& i:board){
-        // for loop which iterates inner vector
-        cout<<"Size of the inner vector is "<<i.size()<<endl;
-        for (auto j:i){
-            cout<<j;
-        }
-        cout<<endl;
+vector<int> ParseLine (const string& line){
+    vector<int> line_val;
+    istringstream line_buf(line);
+    int val;
+    char c;
+
+    while (line_buf >> val >> c){
+        line_val.push_back(val);
     }
-    return 0;
+    return line_val;
+}
+
+void ReadBoardFile(const string& path) {
+    ifstream boardFile(path);
+    if (boardFile) {
+        string line;
+        while (getline(boardFile, line)) {
+            cout << line << "\n";
+        }
+    }
+}
+
+void PrintBoard(const vector<vector<int>>& board) {
+    for(const auto & i : board) {
+        for (int j : i) {
+            cout << j;
+        }
+        cout << "\n";
+    }
+}
+
+int main (){
+    ReadBoardFile(BOARD_FILE_PATH);
+//    vector<vector<int>> board = BOARD_VALUES;
+//    PrintBoard(board);
+    TestParseLine();
+    return EXIT_SUCCESS;
+}
+
+void PrintVector(vector<int> v) {
+    cout << "{ ";
+    for (auto item : v) {
+        cout << item << " ";
+    }
+    cout << "}" << "\n";
+}
+
+//Testing the ParseLine method
+void TestParseLine() {
+    cout << "----------------------------------------------------------" << "\n";
+    cout << "TestParseLine: ";
+    string line = "0,1,0,0,0,0,";
+    vector<int> solution_vect{0,1,0,0,0,0};
+    vector<int> test_vect;
+    test_vect = ParseLine(line);
+    if (test_vect != solution_vect) {
+        cout << "failed" << "\n";
+        cout << "\n" << "Test input string: " << line << "\n";
+        cout << "Your parsed line: ";
+        PrintVector(test_vect);
+        cout << "\n";
+    } else {
+        cout << "passed" << "\n";
+    }
+    cout << "----------------------------------------------------------" << "\n";
+    return;
 }
