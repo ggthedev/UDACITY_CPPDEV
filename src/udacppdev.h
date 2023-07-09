f7b63cfb9159033873a48072ae3025d721e0856a
Gaurav Gupta <1276964+ggthedev@users.noreply.github.com>
2023-07-10 02:46:27 +0530

    added base files for project based

---
+++ src/udacppdev.h
@@ -0,0 +1,14 @@
+//
+// Created by Gaurav Gupta on 10/07/2023.
+//
+
+#ifndef UDACPPDEV_H
+#define UDACPPDEV_H
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#define BOARD_VALUES {{0, 1, 0, 0, 0, 0},{0, 1, 0, 0, 0, 0},{0, 1, 0, 0, 0, 0},{0, 1, 0, 0, 0, 0},{0, 0, 0, 0, 1, 0}};
+
+#endif //UDACPPDEV_H
