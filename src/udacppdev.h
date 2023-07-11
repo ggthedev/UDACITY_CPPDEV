a48a2a26abd0b2b86ab84d0c016a74a2fb7985e1
Gaurav Gupta <1276964+ggthedev@users.noreply.github.com>
2023-07-11 16:07:01 +0530

    updated the code
    all the tests pass
    all the necessary components for subsequent course work are ready

--- src/udacppdev.h
+++ src/udacppdev.h
@@ -18,11 +18,14 @@ using namespace std;
 #define BOARD_VALUES {{0, 1, 0, 0, 0, 0},{0, 1, 0, 0, 0, 0},{0, 1, 0, 0, 0, 0},{0, 1, 0, 0, 0, 0},{0, 0, 0, 0, 1, 0}};
 const string BOARD_FILE_NAME = "1.board";
 const string BOARD_FILE_PATH = string("./../../src/").append(BOARD_FILE_NAME);
-void TestParseLine();
-enum class State {kEmpty, kObstacle, kClosed, kPath};
+enum class State {kEmpty, kObstacle, kClosed, kPath, kStart,kFinish};
 void AddToOpen(int x, int y, int g, int h, vector<vector<int>> &openNodes,vector<vector<State>> &grid);
 void CellSort(vector<vector<int>> *v);
 bool Compare(vector<int> n1,vector<int> n2);
 bool CheckValidCell(int x, int y, vector<vector<State>> &grid);
+int Heuristic(int x1, int y1, int x2, int y2);
+string CellString(State cell);
+vector<vector<State>> ReadBoardFile(const string &path);
+vector<vector<State>> Search(vector<vector<State>> grid, int init[2], int goal[2]);
 void ExpandNeighbors(vector<int> current, int goal[2], vector<vector<int>> &open, vector<vector<State>> &grid);
 #endif //UDACPPDEV_H
